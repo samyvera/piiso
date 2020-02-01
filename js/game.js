5f7c43b837091a4ae93ca68fcea6832c2d96bb6f
samy <samy_leni@hotmail.fr>
2020-02-02 00:01:06 +0100

    feat(dislay): intro text scroll

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ class Game {
     constructor(inputList) {
         this.frame = 0;
 
-        this.introEndFrame = 300;
+        this.introEndFrame = 420;
 
         this.inputList = inputList;
         this.lastInputList = new Map();
