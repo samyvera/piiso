221f3163a09aa45d8210d29299ca3f8b9dd46597
Bigeard <robin.bigeard@gmail.com>
2020-02-01 14:45:14 +0100

    feat(logic): hammer touch player

--- js/player.js
+++ js/player.js
@@ -132,7 +132,7 @@ class Player {
                 }
                 else if (inputs.a && !this.coolDown) {
                     this.addBlock(game);
-                } else if (inputs.b && !this.hammer) {
+                } else if (inputs.b && !this.hammer && !this.coolDown) {
                     this.throwHammer();
                 }
             }
