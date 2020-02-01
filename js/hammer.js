b4894054f5951f27bff0fc5550817e4dc6533259
samy <samy_leni@hotmail.fr>
2020-02-01 14:03:18 +0100

    fix(logic): typo

--- js/hammer.js
+++ js/hammer.js
@@ -1,11 +1,15 @@
 class Hammer {
-    
+
     constructor(pos, direction) {
         this.speed = new Vector3D(0, 0, 0.3);
         this.xSpeed = 0.1;
         // this.degrees = 90;
-        this.direction = {...direction};
-        this.collisionBox = new CollisionBox({...pos}, new Vector3D(0.5, 0.5, 0.5));
+        this.direction = {
+            ...direction
+        };
+        this.collisionBox = new CollisionBox({
+            ...pos
+        }, new Vector3D(0.5, 0.5, 0.5));
         this.isDestroyed = false;
 
         this.moveXY = (game) => {
@@ -41,9 +45,9 @@ class Hammer {
             var newCollisionBox = new CollisionBox(this.collisionBox.pos.plus(new Vector3D(this.speed.x, 0, 0)), this.collisionBox.size);
             if (!newCollisionBox.intersectingCollisionBoxes([...game.scene.blocks.values()]).length) {
                 this.collisionBox = newCollisionBox;
-            } else {
+            } else if (!this.isDestroyed) {
                 this.isDestroyed = true;
-                var blocks = newCollisionBox.intersectingCollisonBoxes([...game.scene.blocks.values()]);
+                var blocks = newCollisionBox.intersectingCollisionBoxes([...game.scene.blocks.values()]);
                 blocks.forEach(block => {
                     game.scene.blocks.delete(Math.floor(block.pos.x) + ", " + Math.floor(block.pos.y) + ", " + Math.floor(block.pos.z));
                 });
@@ -52,7 +56,7 @@ class Hammer {
             var newCollisionBox = new CollisionBox(this.collisionBox.pos.plus(new Vector3D(0, this.speed.y, 0)), this.collisionBox.size);
             if (!newCollisionBox.intersectingCollisionBoxes([...game.scene.blocks.values()]).length) {
                 this.collisionBox = newCollisionBox;
-            } else {
+            } else if (!this.isDestroyed) {
                 this.isDestroyed = true;
                 var blocks = newCollisionBox.intersectingCollisionBoxes([...game.scene.blocks.values()]);
                 blocks.forEach(block => {
@@ -67,7 +71,7 @@ class Hammer {
             var newCollisionBox = new CollisionBox(this.collisionBox.pos.plus(new Vector3D(0, 0, this.speed.z)), this.collisionBox.size);
             if (!newCollisionBox.intersectingCollisionBoxes([...game.scene.blocks.values()]).length && this.collisionBox.pos.z >= 0) {
                 this.collisionBox = newCollisionBox;
-            } else {
+            } else if (!this.isDestroyed) {
                 this.isDestroyed = true;
                 var blocks = newCollisionBox.intersectingCollisionBoxes([...game.scene.blocks.values()]);
                 blocks.forEach(block => {
@@ -84,4 +88,4 @@ class Hammer {
             }
         }
     }
-} 
+}
